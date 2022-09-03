c25e090d54cba56cae5095fbbcceb9caf931ad56
Artem <alexx1088@gmail.com>
2022-09-03 20:08:03 +0600

    add component vue

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,21 @@
+<template>
+
+       <div>
+   <SinglePostComponent></SinglePostComponent>
+    </div>
+</template>
+
+<script>
+    import SinglePostComponent from "./SinglePostComponent";
+    export default {
+        name: "PostComponent",
+
+        components: {
+            SinglePostComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
